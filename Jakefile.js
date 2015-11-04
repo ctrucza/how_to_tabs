dffe8569df0a0891b862f69b36719eaec55c167d
Csaba Trucza <csaba.trucza@gmail.com>
2015-11-04 17:29:29 +0200

    Build now checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,19 @@
 (function (){
     "use strict";
 
+    var expected_node_version = "v4.2.1";
+
     desc("Default build");
-    task("default", function(){
+    task("default", ["version"], function(){
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version");
+    task("version", function(){
+        console.log("Checking Node version");
+
+        let actual_node_version = process.version;
+        if (actual_node_version !== expected_node_version)
+            fail("Incorrect Node version: expected " + expected_node_version + " but was " + actual_node_version);
     });
 }());

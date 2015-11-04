95b32d33ef131f3418a003b2e149308cbfc644db
Csaba Trucza <csaba.trucza@gmail.com>
2015-11-04 17:48:01 +0200

    Using semver to check Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,7 @@
 (function (){
     "use strict";
 
+    var semver = require("semver");
 
     desc("Default build");
     task("default", ["version"], function(){
@@ -14,7 +15,7 @@
         var package_json = require("./package.json");
         var expected_node_version = package_json.engines.node;
         let actual_node_version = process.version;
-        if (actual_node_version !== "v" + expected_node_version)
+        if (semver.neq(actual_node_version, expected_node_version))
             fail("Incorrect Node version: expected " + expected_node_version + " but was " + actual_node_version);
     });
 }());

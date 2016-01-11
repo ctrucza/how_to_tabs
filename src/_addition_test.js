6ed45e7f44e86ba34a02e5aa58ecc76158237048
Csaba Trucza <csaba.trucza@gmail.com>
2016-01-11 17:46:13 +0200

    factored out example code from test

--- src/_addition_test.js
+++ src/_addition_test.js
@@ -9,18 +9,16 @@
         }
     };
 
+    var addition = require("./addition.js");
+
     describe("Addition", function(){
         it("adds positive numbers", function(){
-            assert.equal(add(3, 4), 7);
+            assert.equal(addition.add(3, 4), 7);
         });
 
         it("uses IEEE 754 floating point", function(){
-            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+            assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
         });
     });
 
-
-    function add(a, b){
-        return a + b;
-    }
 }());

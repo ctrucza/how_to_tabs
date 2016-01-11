ad68bbafa7917d3c60de117d0cf1907b3755599e
Csaba Trucza <csaba.trucza@gmail.com>
2016-01-11 13:04:48 +0200

    configured Karma to run our tests in mocha

--- src/test.js
+++ src/test.js
@@ -1,7 +1,13 @@
 (function () {
     "use strict";
 
-    var assert = require("chai").assert;
+    //var assert = require("chai").assert;
+
+    var assert = {
+        equal: function equal(actual, expected){
+            if (actual !== expected) throw new Error("expected " + expected + " but was " + actual);
+        }
+    }
 
     describe("Addition", function(){
         it("adds positive numbers", function(){

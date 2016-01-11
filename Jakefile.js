624dfd0cdedaf8288f0d276ed604f66e02962dc1
Csaba Trucza <csaba.trucza@gmail.com>
2016-01-11 13:43:32 +0200

    we can jake loose=true to prevent build fail when not all browsers are tested

--- Jakefile.js
+++ Jakefile.js
@@ -64,7 +64,8 @@
             expectedBrowsers:[
                 "Chrome 47.0.2526 (Windows 10 0.0.0)",
                 "Edge 13.10586.0 (Windows 10 0.0.0)"
-            ]
+            ],
+            strict: !process.env.loose
         }, complete, fail);
     }, {async: true});
 

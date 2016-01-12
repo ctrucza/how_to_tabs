0907038237649b175f377f8e608e4eb824800374
Csaba Trucza <csaba.trucza@gmail.com>
2016-01-12 08:12:18 +0200

    Created empty tabs library

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -6,8 +6,14 @@
 
     describe("Tabs", function(){
 
-        it("has an API", function(){
-            tabs.initialize();
+        it("hides an element", function(){
+            var element = document.createElement("div");
+
+            tabs.initialize(element);
+
+            var styles = getComputedStyle(element);
+            var display = styles.getPropertyValue("display");
+            assert.equal(display, "none");
         });
 
     });
